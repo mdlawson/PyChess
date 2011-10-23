451b3bda52c71f034c88d14ff349d0edd2621c22
Michael Lawson <mdlawson@gmx.com>
2011-10-23 23:54:43 +0100

    mappingLoop "appears" to work, tested with rook. However, weird wierd bug thats breaking my brain right now, test output shows it finding valid moves on totally the wrong row, yet everything else seems fine.

--- Pieces.py
+++ Pieces.py
@@ -1,40 +1,82 @@
 class Rook:
-	def isLegal(move):
-		if move[0] or move[1] =0:
+	def isLegal(self, move):
+		if move[0] == 0 or move[1] == 0:
 			return 0
 		else:
 			return 1
+	def moves(self, pos):
+		return mappingLoop(pos, self)
 class Bishop:
 	def isLegal(move):
 		if abs(move[0])==abs(move[1]):
 			return 0
 		else:
 			return 1
+	def moves(pos):
+		return mappingLoop(pos, self)
 class Knight:
 	def isLegal(move):
 		move = [abs(move[0]),abs(move[1])]
-		if move == [1,2] or [2,1]:
+		if move == [1,2] or move == [2,1]:
 			return 0
 		else:
 			return 1
+	def moves(pos):
+		return mappingLoop(pos, self)
 class King:
 	def isLegal(move):
 		move = [abs(move[0]),abs(move[1])]
-		if move == [1,0] or [0,1] or [1,1]:
+		if move == [1,0] or move == [0,1] or move == [1,1]:
 			return 0
 		else:
 			return 1
+	def moves(pos):
+		return mappingLoop(pos, self)
 class Queen:
 	def isLegal(move):
 		if Bishop.isLegal(move) or Rook.isLegal(move):
 			return 0
 		else:
 			return 1
-class Pawn:
+	def moves(pos):
+		return mappingLoop(pos, self)
 
+def mappingLoop(pos, piece):
+	valid = []
+	for x in range(8):
+		for y in range(8):
+			print "Mapping [",x,",",y,"]...", 
+			if piece.isLegal([(y-pos[0]),(x-pos[1])]) == 0:
+				print "Valid!"
+				valid.append([y,x])
+			else:
+				print "Invalid!"
+	return valid
 
+#def checkingLoop(from, move, board):
+#	if move[0] < 0:
+#		yRange = range(0, move[0], -1)
+#	else:
+#		yRange = range(move[0])
+#	if move[1] < 0:
+#		xRange = range(0, move[1], -1)
+#	else:
+#		xRange = range(move[1])
+#	for x in xRange for y in yRange:
+#		print x, ",", y
+#		if board[from[0]+y][from[1]+x] != "  "
+#			return 1
 	
 
-
-
-		
+board = [									# This is the original board
+["  ","  ","  ","  ","  ","  ","  ","  "],
+["  ","  ","  ","  ","  ","  ","  ","  "],	# I changed bishops and knights the right way round
+["  ","  ","  ","  ","  ","  ","  ","  "],	# N = Knights, as in normal chess notation, "N"
+["  ","  ","  ","1r","  ","2p","  ","  "],
+["  ","  ","  ","  ","  ","  ","  ","  "],
+["  ","  ","  ","  ","  ","  ","  ","  "],
+["  ","  ","  ","  ","  ","  ","  ","  "],
+["  ","  ","  ","  ","  ","  ","  ","  "]
+]
+WR1 = Rook()
+mappingLoop([3,4],WR1)
