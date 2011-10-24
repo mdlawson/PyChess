82730df16971d36c7ae91a36a92b8245764260ed
cchristiansen <christcl01@hotmail.com>
2011-10-24 17:12:18 +0100

    Added black

--- Pieces.py
+++ Pieces.py
@@ -2,8 +2,8 @@ from itertools import izip_longest
 
 class Rook: # This is a class for a piece
 	def __init__(self, pos, team): # when a new object of this class is made, its position needs to be supplied
-		self.pos = pos
-		self.team = team #pieces position is saved as Piece.pos
+		self.pos = pos #pieces position is saved as Piece.pos
+		self.team = team 
 	def isLegal(self, move): # Piece.isLegal checks if a move is legal for th currect piece. takes 1 arg, as self is always supplied
 		if move[0] == 0 or move[1] == 0:
 			return 0
@@ -98,26 +98,30 @@ def checkLegal(piece, move):
 	if piece.isLegal(move) != 0:
 		return 1
 	
-pieces = {'R':Rook,'N':Knight,'B':Bishop,'Q':Queen,'K':King} # A dictionary for translating piece short codes to piece classes	
+pieces = {'R':Rook,'N':Knight,'B':Bishop,'Q':Queen,'K':King} # A dictionary for translating piece short codes to piece classes
+piece = {}
+teams = {'White':1,'Black':2}
 
 board = [									# This is a testing board
-["  ","  ","  ","  ","  ","  ","  ","  "],
-["  ","  ","  ","  ","  ","  ","  ","  "],
-["  ","  ","  ","  ","  ","  ","  ","  "],	
-["  ","  ","  ","  ","  ","  ","  ","  "],
-["  ","  ","  ","  ","  ","  ","  ","  "],
-["  ","  ","  ","  ","  ","  ","  ","  "],
-["  ","  ","  ","  ","  ","  ","  ","  "],
-["1R1","1N1","1B1","1Q1","1K1","  ","  ","  "]
+["2R1","2N1","2B1","2Q1","2K1","2B2","2N2","2R2"],
+["2p1","2p2","2p3","2p4","2p5","2p6","2p7","2p8"],
+["   ","   ","   ","   ","   ","   ","   ","   "],	
+["   ","   ","   ","   ","   ","   ","   ","   "],
+["   ","   ","   ","   ","   ","   ","   ","   "],
+["   ","   ","   ","   ","   ","   ","   ","   "],
+["1p1","1p2","1p3","1p4","1p5","1p6","1p7","1p8"],
+["1R1","1N1","1B1","1Q1","1K1","1B2","1N2","1R2"]
 ]
 def setupPieces(board): # this is an init type function, sets up all the pieces on the board by calling them shortcode given on the board. problems with multiple pieces on same team needing uniques. 
 	for y in range(len(board)):
 		for x in range(len(board[y])):
-			if board[y][x] != "  ":
+			if board[y][x] != "   " and board[y][x][1] != "p":
 				print "Found piece",board[y][x],", creating new",pieces[board[y][x][-2]]
-				vars()['board[y][x]'] = (pieces[board[y][x][-2]])([y, x],board[y][x][0])
-				vars()['board[y][x]'].sayHi()
-	print(board)
-#mappingLoop([3,4],WR1) # various testing stubs :P 
+				piece[board[y][x]] = (pieces[board[y][x][-2]])([y, x],board[y][x][0])
+				piece[board[y][x]].sayHi()
+				print board[y][x]
+#	print(board)
+	piece["1R2"]
+#mappingLoop() # various testing stubs :P 
 #checkingLoop([3,3],[-3,0],board)
 setupPieces(board)
