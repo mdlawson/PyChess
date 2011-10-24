70f4d3f84b9fdf2ea11d1c54b02411d85056e8ec
Michael Lawson <mdlawson@gmx.com>
2011-10-24 13:13:14 +0100

    Added piecesSetup, which adds all the pieces on the board. Intergrated position into a Piece, so each Piece knows where it is. Unsure about how neat this "new way" is. Also, new method brings up issues of unique piece naming.

--- Pieces.py
+++ Pieces.py
@@ -1,54 +1,74 @@
 from itertools import izip_longest
 
 class Rook:
+	def __init__(self, pos):
+		self.pos = pos
 	def isLegal(self, move):
 		if move[0] == 0 or move[1] == 0:
 			return 0
 		else:
 			return 1
-	def moves(self, pos):
-		return mappingLoop(pos, self)
+	def moves(self):
+		return mappingLoop(self)
+	def sayHi(self):
+		print "Hi, I'm a rook! I'm located at:",self.pos
 class Bishop:
+	def __init__(self, pos):
+		self.pos = pos
 	def isLegal(self, move):
 		if abs(move[0])==abs(move[1]):
 			return 0
 		else:
 			return 1
-	def moves(self, pos):
-		return mappingLoop(pos, self)
+	def moves(self):
+		return mappingLoop(self)
+	def sayHi(self):
+		print "Hi, I'm a Bishop! I'm located at:",self.pos
 class Knight:
+	def __init__(self, pos):
+		self.pos = pos
 	def isLegal(self, move):
 		move = [abs(move[0]),abs(move[1])]
 		if move == [1,2] or move == [2,1]:
 			return 0
 		else:
 			return 1
-	def moves(self, pos):
-		return mappingLoop(pos, self)
+	def moves(self):
+		return mappingLoop(self)
+	def sayHi(self):
+		print "Hi, I'm a Knight! I'm located at:",self.pos
 class King:
+	def __init__(self, pos):
+		self.pos = pos
 	def isLegal(self, move):
 		move = [abs(move[0]),abs(move[1])]
 		if move == [1,0] or move == [0,1] or move == [1,1]:
 			return 0
 		else:
 			return 1
-	def moves(self, pos):
-		return mappingLoop(pos, self)
+	def moves(self):
+		return mappingLoop(self)
+	def sayHi(self):
+		print "Hi, I'm the King! I'm located at:",self.pos
 class Queen:
+	def __init__(self, pos):
+		self.pos = pos
 	def isLegal(self, move):
 		if Bishop.isLegal(self, move) or Rook.isLegal(self, move):
 			return 0
 		else:
 			return 1
-	def moves(self, pos):
-		return mappingLoop(pos, self)
+	def moves(self):
+		return mappingLoop(self)
+	def sayHi(self):
+		print "Hi, I'm the queen! I'm located at:",self.pos
 
-def mappingLoop(pos, piece):
+def mappingLoop(piece):
 	valid = []
 	for x in range(8):
 		for y in range(8):
 			print "Mapping [",x,",",y,"]...", 
-			if piece.isLegal([(y-pos[0]),(x-pos[1])]) == 0:
+			if piece.isLegal([(y-piece.pos[0]),(x-piece.pos[1])]) == 0:
 				print "Valid!"
 				valid.append([y,x])
 			else:
@@ -68,18 +88,27 @@ def checkingLoop(posFrom, move, board):
 		print y, ",", x
 		if board[posFrom[0]+y][posFrom[1]+x] != "  ":
 			print "there is a piece in the way!"
-	
+
+pieces = {'r':Rook,'n':Knight,'b':Bishop,'Q':Queen,'K':King}	
 
 board = [									# This is the original board
 ["  ","  ","  ","  ","  ","  ","  ","  "],
 ["  ","  ","  ","  ","  ","  ","  ","  "],	# I changed bishops and knights the right way round
 ["  ","  ","  ","  ","  ","  ","  ","  "],	# N = Knights, as in normal chess notation, "N"
-["  ","  ","hh","1r","  ","2p","  ","  "],
 ["  ","  ","  ","  ","  ","  ","  ","  "],
 ["  ","  ","  ","  ","  ","  ","  ","  "],
 ["  ","  ","  ","  ","  ","  ","  ","  "],
-["  ","  ","  ","  ","  ","  ","  ","  "]
+["  ","  ","  ","  ","  ","  ","  ","  "],
+["1r","1n","1b","1Q","1K","  ","  ","  "]
 ]
-WR1 = Rook()
+def setupPieces(board):
+	for y in range(len(board)):
+		for x in range(len(board[y])):
+			if board[y][x] != "  ":
+				print "Found piece",board[y][x],", creating new",pieces[board[y][x][-1]]
+				board[y][x] = (pieces[board[y][x][-1]])([y, x])
+				board[y][x].sayHi()
+#WR1 = Rook()
 #mappingLoop([3,4],WR1)
-checkingLoop([3,3],[-3,0],board)
+#checkingLoop([3,3],[-3,0],board)
+setupPieces(board)
